7670bd669aca76cbd1b05987712a40195e6f9504
Guduri Sriram <guduri.sriram@inmobi.com>
2017-11-12 17:09:08 +0530

    added leetcode

---
+++ leetcode/java/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Map<Integer, Integer> map = new HashMap<>();
+        IntStream.range(0,5).forEach(i -> {
+            if (map.get(1) == null) {
+                map.put(1, 1);
+            } else {
+                map.put(1, map.get(1)+1);
+            }
+        });
+        System.out.println(map.get(1));
+    }
+}
